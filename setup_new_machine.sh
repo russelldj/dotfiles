42e82cf21e5746aa9357c05d98c1452ccce09c51
David Russell <russeldj@clarkson.edu>
2019-01-20 12:05:00 -0500

    set up ssh keys if not present

--- setup_new_machine.sh
+++ setup_new_machine.sh
@@ -15,3 +15,10 @@ source link_dotfiles.sh
 # set up some useful git stuff
 git config --global user.email "russeldj@clarkson.edu"
 git config --global user.name  "David Russell"
+
+# set up ssh keys
+# check if they exist already
+if [ ! -f ~/.ssh/id_rsa.pub ]; then 
+	# if not, create them
+	ssh-keygen -t rsa
+fi

c9a6309fafa89024a8c3074e8a0ea02258ae539d
David Russell <russeldj@clarkson.edu>
2019-01-20 14:40:42 -0500

    added i3 config files

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,22 +1,26 @@
+## setting up the shell
+
 #set this to false if you are trying to set up bash
 USE_ZSH=true
 
+if $USE_ZSH; then
+    echo "Setting up zsh"
+    mv ~/.zshrc ~/.OLDzshrc
+    ln -s $(pwd)/.zshrc ~/
+    source ~/.zshrc
+else
+    echo "Setting up bash"
+    mv ~/.bashrc ~/.OLDbashrc
+    ln -s $(pwd)/.bashrc ~/
+    source ~/.bashrc
+fi
 
-#if $USE_ZSH; then
-#    echo "Setting up zsh"
-#    mv ~/.zshrc ~/.OLDzshrc
-#    ln -s $(pwd)/.zshrc ~/
-#    source ~/.zshrc
-#else
-#    echo "Setting up bash"
-#    mv ~/.bashrc ~/.OLDbashrc
-#    ln -s $(pwd)/.bashrc ~/
-#    source ~/.bashrc
-#fi
-
+## setting up ssh
 if [ ! -f ~/.ssh/config ]; then 
 	# if not, create them
 	ln -s $(pwd)/sshconfig ~/.ssh/config 
-	
 fi
 
+## I3
+mv ~/.config/i3/config ~/.config/i3/OLDconfig
+ln -s $(pwd)/i3config ~/.config/i3/config
